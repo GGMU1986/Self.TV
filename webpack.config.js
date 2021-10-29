d6a78f89d7ce1a701d20a2e88a38769c98df974d
ggmu1986 <gt2186@gmail.com>
2021-10-29 15:32:40 -0400

    React set up, rendering things to browser

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/bench_bnb.jsx',
+  entry: './frontend/self_tv.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
@@ -14,7 +14,7 @@ module.exports = {
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
-          query: {
+          options: {
             presets: ['@babel/env', '@babel/react']
           }
         },
