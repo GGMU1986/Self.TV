57a364d88ba2d37703c3f4331b42a88eddddb45d
ggmu1986 <gt2186@gmail.com>
2021-10-29 16:56:18 -0400

    Set up users and sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,25 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login!(user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 401
+    end
+  end
+
+  def destroy
+    if current_user
+      logout!
+      render :show
+    else
+      render json: ["Nobody signed in"], status: 404
+    end
+  end
 end
