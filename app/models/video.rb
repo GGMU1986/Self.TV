23b74cd8b48ecffeed1a4750c291d55259858dd7
ggmu1986 <gt2186@gmail.com>
2021-10-31 13:57:12 -0400

    Added video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,12 @@
 class Video < ApplicationRecord
+
+  validates :title, presence:true
+
+  belongs_to :user,
+    foreign_key: :user_id,
+    class_name: :User
+  
+    belongs_to :channel,
+      foreign_key: :channel_id,
+      class_name: :Channel
 end
