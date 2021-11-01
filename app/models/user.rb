2f36e09b9de00741a67fd6a77407ba03e022121a
ggmu1986 <gt2186@gmail.com>
2021-11-01 17:48:42 -0400

    Upgraded bundler

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
     foreign_key: :uploader_id,
     class_name: :Video
   
-    has_one_attached :photo
+  has_one_attached :photo
 
   after_initialize :ensure_session_token
 
